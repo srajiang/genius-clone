69203619da862fd49786ee3a118f089abe1b414e
srajiang <srajiang@gmail.com>
2020-01-07 17:05:46 -0800

    Morebugfixing

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,39 +1,39 @@
 class Api::SongsController < ApplicationController
-  before_action :set_report, only: [:show, :update]
+  # before_action :set_report, only: [:show, :update]
   
-  def index
-    @songs = Song.all
-  end
+  # def index
+  #   @songs = Song.all
+  # end
 
-  def show
-  end
+  # def show
+  # end
 
-  def create
-    @song = Song.new(song_params)
+  # def create
+  #   @song = Song.new(song_params)
     
-    if @song.save
-      render :show
-    else
-      render json: @song.errors.full_messages, status: 422
-    end
-  end
+  #   if @song.save
+  #     render :show
+  #   else
+  #     render json: @song.errors.full_messages, status: 422
+  #   end
+  # end
 
-  def update
-    if @song.update(song_params)
-      render :show
-    else
-      render :json @song.errors.full_messages, status: 422
-    end
-  end
+  # def update
+  #   if @song.update(song_params)
+  #     render :show
+  #   else
+  #     render :json @song.errors.full_messages, status: 422
+  #   end
+  # end
 
-  private
-  def song_params
-  end
+  # private
+  # def song_params
+  # end
 
-  def set_report
-    @song = Song.find(params[:id])
-  rescue
-    render json: ['Song not found'], status: :not_found
-  end
+  # def set_report
+  #   @song = Song.find(params[:id])
+  # rescue
+  #   render json: ['Song not found'], status: :not_found
+  # end
   
 end
